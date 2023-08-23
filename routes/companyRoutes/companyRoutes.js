e11e1f41d5a543b5118c41c76d31c7bdd8b6d0a6
developerweb004 <140089167+DevelopersWeb004@users.noreply.github.com>
2023-08-23 09:30:53 +0530

    updated userContriller.js file
    added the functionality of invite the team member with email invitation and adding the data into the database and sending the invitation email.

--- routes/companyRoutes/companyRoutes.js
+++ routes/companyRoutes/companyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isAuthenticatedUser, authorizeRoles } = require("../../middleware/auth.js");
-const { login, registerUser, forgotPassword, resetPassword, getCompanyDetails, getUsers, getUserDetails, getProfile, logout, updateTeam, updateStatus, inviteTeamMember } = require('../../controllers/customers/userController.js');
+const { login, registerUser, forgotPassword, resetPassword, getCompanyDetails, getUsers, getUserDetails, getProfile, logout, updateTeam, updateStatus, inviteTeamMember, getinvitedUsers } = require('../../controllers/customers/userController.js');
 
 const router = express.Router();
 
@@ -12,6 +12,7 @@ router.put('/reset/password/:token',resetPassword)
 router.post('/register/new',registerUser)
 router.get('/company/profile',isAuthenticatedUser,getCompanyDetails);
 router.get('/users',isAuthenticatedUser,getUsers)
+router.get('/invitedusers',isAuthenticatedUser,getinvitedUsers)
 router.get('/profile',isAuthenticatedUser,getProfile)
 router.get('/user/:id',isAuthenticatedUser,getUserDetails)
 router.put('/user/update/team',isAuthenticatedUser,updateTeam)
