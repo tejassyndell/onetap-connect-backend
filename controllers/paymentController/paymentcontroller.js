d1a90b64d965cff38644d155bb8ab433c6abff1c
dev-4 <developersweb004@gmail.com>
2023-08-23 13:47:15 +0530

    made stripe payment gateway dynamic

--- controllers/paymentController/paymentcontroller.js
+++ controllers/paymentController/paymentcontroller.js
@@ -1,16 +1,30 @@
 const catchAsyncErrors = require("../../middleware/catchAsyncErrors");
 const dotenv = require('dotenv');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 dotenv.config()
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
+  console.log(req.body)
+  const Address = req.body.billingAddress;
 
   const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount * 100,
-    currency: "inr",
+    currency: "usd",
+    description: 'Test description', // Provide an export-related description
     metadata: {
-      company: "Test",
+      company: req.body.company_name,
+    },
+    shipping: {
+      address: {
+        line1: Address.Bstreet1,
+        line2: Address.Bstreet2,
+        city: Address.Bcity,
+        state: Address.Bstate,
+        postal_code: Address.BpostalCode,
+        country: Address.Bstate,
+      },
+      name: req.body.name
     },
   });
   
