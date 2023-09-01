c8bcc3a4d0b66873a282818f2f7e5223068743f1
developer04 <developersweb001@gmail.com>
2023-09-01 14:31:59 +0530

    add rename and delete team apis

--- routes/companyRoutes/companyRoutes.js
+++ routes/companyRoutes/companyRoutes.js
@@ -3,7 +3,7 @@ const { isAuthenticatedUser, authorizeRoles } = require("../../middleware/auth.j
 const { login,
      registerUser,
      forgotPassword, 
-     resetPassword, getCompanyDetails, getUsers, getUserDetails, getProfile, logout, updateTeam, updateStatus,updateUserDetails, inviteTeamMember, getinvitedUsers, signUP1, signUP2, addCardDetails, showCardDetails, updateBillingAddress, createNewTeam, updateTeamName, checkslugavailiblity,updateCompanyDetails, removeTeamFromUsers, updateCompanyDetailsInfo, checkoutHandler} = require('../../controllers/customers/userController.js');
+     resetPassword, getCompanyDetails, getUsers, getUserDetails, getProfile, logout, updateTeam, updateStatus,updateUserDetails, inviteTeamMember, getinvitedUsers, signUP1, signUP2, addCardDetails, showCardDetails, updateBillingAddress, createNewTeam, updateTeamName, checkslugavailiblity,updateCompanyDetails, removeTeamFromUsers, updateCompanyDetailsInfo, checkoutHandler, renameTeam, deleteTeam} = require('../../controllers/customers/userController.js');
 
 const router = express.Router();
 
@@ -29,6 +29,9 @@ router.get('/invitedusers', isAuthenticatedUser, getinvitedUsers)
 router.post("/user/update/billingAddress",isAuthenticatedUser,updateBillingAddress);
 router.put("/user/update/users/team",isAuthenticatedUser, updateTeamName);
 router.post('/user/create/team',isAuthenticatedUser,createNewTeam)
+router.post("/user/rename/team", isAuthenticatedUser, renameTeam)
+router.post("/user/delete/team", isAuthenticatedUser, deleteTeam)
+
 // router.post('/check-availability', isAuthenticatedUser,checkslugavailiblity)
 router.put("/company/update",isAuthenticatedUser,updateCompanyDetails);
 router.put("/company/update/information",isAuthenticatedUser,updateCompanyDetailsInfo);
