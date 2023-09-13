ba03b502230ca85b02a86259eb5d98c64a84bcff
developersweb05 <developersweb003@gmail.com>
2023-09-13 18:38:44 +0530

    updated setting page

--- models/NewSchemas/Company_informationModel.js
+++ models/NewSchemas/Company_informationModel.js
@@ -1,87 +1,101 @@
 const mongoose = require("mongoose");
 
 const Company_information = new mongoose.Schema(
-    {
-        company_name: {
-            type: String,
-            required: [true, "Please Enter Your Company Name"],
-        },
-        contact: { type: Number, default: null },
-        fax_number: {
-            type: Number,
-            // required: [true, "Please Enter The Company fax Number"],
-        },
-        address: {
-            line1: { type: String, default: null },
-            line2: { type: String, default: null },
-            city: { type: String, default: null },
-            state: { type: String, default: null },
-            country: { type: String, default: null },
-            postal_code: { type: String, default: null },
-        },
-        global_email: {
-            type: String,
-            // required: [true, "Please Enter The Company email"],
-        },
-        industry: { type: String },
-        keywords: [{ type: String }],
-        website_url: {
-            type: String,
-            // required: [true, "Please Enter The Company URL"],
-        },
-        about: {
-            type: String,
-            default: null
-        },
-        videoAboutUrl: {
-            type: String,
-            default: null
-        },
-        timeZone: { type: String, default: "" },
-        booking_links: [
-            {
-                value: { type: String, default: null },
-                permission: { type: Boolean, default: false },
-            },
-        ],
-        other_links: [
-            {
-                title: { type: String, default: null },
-                value: { type: String, default: null },
-                permission: { type: Boolean, default: false },
-            },
-        ],
-        custom_fields: [
-            {
-                title: { type: String, default: null },
-                value: { type: String, default: null },
-                permission: { type: Boolean, default: false },
-            },
-        ],
-        socialLinks: [
-            {
-                title: { type: String, default: null },
-                value: { type: String, default: null },
-                permission: { type: Boolean, default: false },
-            },
-        ],
-        primary_account: {
-            type: mongoose.Schema.ObjectId,
-            required: true,
-            ref: "user",
-        },
-        primary_billing: [
-            {
-                type: mongoose.Schema.ObjectId,
-                ref: "user",
-            },
-        ],
-        primary_manager: {
-            type: mongoose.Schema.ObjectId,
-            ref: "user",
-        },
+  {
+    company_name: {
+      type: String,
+      required: [true, "Please Enter Your Company Name"],
     },
-    { timestamps: true }
+    contact: { type: Number, default: null },
+    fax_number: {
+      type: Number,
+      // required: [true, "Please Enter The Company fax Number"],
+    },
+    address: {
+      line1: { type: String, default: null },
+      line2: { type: String, default: null },
+      city: { type: String, default: null },
+      state: { type: String, default: null },
+      country: { type: String, default: null },
+      postal_code: { type: String, default: null },
+    },
+    global_email: {
+      type: String,
+      // required: [true, "Please Enter The Company email"],
+    },
+    industry: { type: String },
+    companyurlslug: {
+      type: String,
+      default: function () {
+        return this.company_name.toLowerCase().replace(/[^a-z0-9-]/g, "");
+      },
+    },
+    keywords: [{ type: String }],
+    website_url: {
+      type: String,
+      // required: [true, "Please Enter The Company URL"],
+    },
+    about: {
+      type: String,
+      default: null,
+    },
+    videoAboutUrl: {
+      type: String,
+      default: null,
+    },
+    timeZone: { type: String, default: "" },
+    booking_links: [
+      {
+        value: { type: String, default: null },
+        permission: { type: Boolean, default: false },
+      },
+    ],
+    other_links: [
+      {
+        title: { type: String, default: null },
+        value: { type: String, default: null },
+        permission: { type: Boolean, default: false },
+      },
+    ],
+    custom_fields: [
+      {
+        title: { type: String, default: null },
+        value: { type: String, default: null },
+        permission: { type: Boolean, default: false },
+      },
+    ],
+    socialLinks: [
+      {
+        title: { type: String, default: null },
+        value: { type: String, default: null },
+        permission: { type: Boolean, default: false },
+      },
+    ],
+    primary_account: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    primary_billing: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+      },
+    ],
+    primary_manager: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+    },
+    user_profile_edit_permission: {
+      type: Boolean,
+      default: true,
+    },
+    company_url_edit_permission: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("companies_information", Company_information);
--- routes/companyRoutes/companyRoutes.js
+++ routes/companyRoutes/companyRoutes.js
@@ -1,21 +1,71 @@
-const express = require('express');
-const { isAuthenticatedUser, authorizeRoles } = require("../../middleware/auth.js");
-const { login,
-     registerUser,
-     forgotPassword, 
-     resetPassword, getCompanyDetails, getUsers,uploadLogo, uploadfavicon, getUserDetails, getProfile, logout, updateTeam, updateStatus,updateUserDetails, inviteTeamMember, getinvitedUsers, signUP1, signUP2, addCardDetails, showCardDetails, updateBillingAddress, createNewTeam, updateTeamName, checkslugavailiblity,updateCompanyDetails, removeTeamFromUsers, updateCompanyDetailsInfo, checkoutHandler,googleSignUP, googleLogin, renameTeam, deleteTeam, checkcompanyurlslugavailiblity, updateCompanySlug, updateAutoRenewal, inviteTeamMemberByCSV,  uploadProfilePicture,
-     addTeamMemberManually,
-     deleteCardDetails,
-     fetchCardDetails,
-     updateCardDetails, createShippingAddress, invitedUserGoogleSignup, registerInvitedUser, invitedUser, getcompanies_share_referral_datas, updatecompany_referral_data,deleteInvitedUser, getAllShippingAddress, removeShippingAddress, editShippingAddress , resendemailinvitation, rejectInvitation} = require('../../controllers/customers/userController.js');
+const express = require("express");
+const {
+  isAuthenticatedUser,
+  authorizeRoles,
+} = require("../../middleware/auth.js");
+const {
+  login,
+  registerUser,
+  forgotPassword,
+  resetPassword,
+  getCompanyDetails,
+  getUsers,
+  uploadLogo,
+  uploadfavicon,
+  getUserDetails,
+  getProfile,
+  logout,
+  updateTeam,
+  updateStatus,
+  updateUserDetails,
+  inviteTeamMember,
+  getinvitedUsers,
+  signUP1,
+  signUP2,
+  addCardDetails,
+  showCardDetails,
+  updateBillingAddress,
+  createNewTeam,
+  updateTeamName,
+  checkslugavailiblity,
+  updateCompanyDetails,
+  removeTeamFromUsers,
+  updateCompanyDetailsInfo,
+  checkoutHandler,
+  googleSignUP,
+  googleLogin,
+  renameTeam,
+  deleteTeam,
+  checkcompanyurlslugavailiblity,
+  updateCompanySlug,
+  updateAutoRenewal,
+  inviteTeamMemberByCSV,
+  uploadProfilePicture,
+  addTeamMemberManually,
+  deleteCardDetails,
+  fetchCardDetails,
+  updateCardDetails,
+  createShippingAddress,
+  invitedUserGoogleSignup,
+  registerInvitedUser,
+  invitedUser,
+  getcompanies_share_referral_data,
+  updatecompany_referral_data,
+  deleteInvitedUser,
+  getAllShippingAddress,
+  removeShippingAddress,
+  editShippingAddress,
+  resendemailinvitation,
+  rejectInvitation,
+} = require("../../controllers/customers/userController.js");
 
 const router = express.Router();
 
-router.post('/register', signUP1)
-router.post('/register/step-2/:token', signUP2)
-router.post('/google-sign-up',googleSignUP)
-router.post('/google-login',googleLogin)
-router.post('/checkout', isAuthenticatedUser,checkoutHandler)
+router.post("/register", signUP1);
+router.post("/register/step-2/:token", signUP2);
+router.post("/google-sign-up", googleSignUP);
+router.post("/google-login", googleLogin);
+router.post("/checkout", isAuthenticatedUser, checkoutHandler);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/forgot/password", forgotPassword);
@@ -33,54 +83,81 @@ router.post("/updateCardDetails/:id", isAuthenticatedUser, updateCardDetails);
 router.get("/showCardDetails", isAuthenticatedUser, showCardDetails);
 router.get("/fetchCardDetails/:id", isAuthenticatedUser, fetchCardDetails);
 router.post("/deleteCardDetails/:id", isAuthenticatedUser, deleteCardDetails);
-router.post('/invite/user', isAuthenticatedUser, inviteTeamMember);
-router.delete('/invited-users/:invitedUserID', deleteInvitedUser);
+router.post("/invite/user", isAuthenticatedUser, inviteTeamMember);
+router.delete("/invited-users/:invitedUserID", deleteInvitedUser);
 
 // router.post('/add/member/manually',isAuthenticatedUser,addTeamMemberManually);
 
-router.get('/invitedusers', isAuthenticatedUser, getinvitedUsers)
-router.post("/update/billingAddress",isAuthenticatedUser,updateBillingAddress);
-router.put("/user/update/users/team",isAuthenticatedUser, updateTeamName);
-router.post('/user/create/team',isAuthenticatedUser,createNewTeam)
-router.post("/user/rename/team", isAuthenticatedUser, renameTeam)
-router.post("/user/delete/team", isAuthenticatedUser, deleteTeam)
+router.get("/invitedusers", isAuthenticatedUser, getinvitedUsers);
 router.post(
-     "/upload-profile-picture/:id",
-     isAuthenticatedUser,
-     uploadProfilePicture
-   );
-   router.post(
-     "/uploadlogo",
-     isAuthenticatedUser,
-     uploadLogo
-   );
-   router.post(
-     "/uploadfavicon",
-     isAuthenticatedUser,
-     uploadfavicon
-   );
+  "/update/billingAddress",
+  isAuthenticatedUser,
+  updateBillingAddress
+);
+router.put("/user/update/users/team", isAuthenticatedUser, updateTeamName);
+router.post("/user/create/team", isAuthenticatedUser, createNewTeam);
+router.post("/user/rename/team", isAuthenticatedUser, renameTeam);
+router.post("/user/delete/team", isAuthenticatedUser, deleteTeam);
+router.post(
+  "/upload-profile-picture/:id",
+  isAuthenticatedUser,
+  uploadProfilePicture
+);
+router.post("/uploadlogo", isAuthenticatedUser, uploadLogo);
+router.post("/uploadfavicon", isAuthenticatedUser, uploadfavicon);
 // router.post('/check-availability', isAuthenticatedUser,checkslugavailiblity)
-router.put("/company/update",isAuthenticatedUser,updateCompanyDetails);
-router.put("/company/update/information",isAuthenticatedUser,updateCompanyDetailsInfo);
-router.post('/user/remove/team',isAuthenticatedUser,removeTeamFromUsers)
+router.put("/company/update", isAuthenticatedUser, updateCompanyDetails);
+router.put(
+  "/company/update/information",
+  isAuthenticatedUser,
+  updateCompanyDetailsInfo
+);
+router.post("/user/remove/team", isAuthenticatedUser, removeTeamFromUsers);
 router.post("/updatecompanyslug", updateCompanySlug);
-router.post('/check-availability', isAuthenticatedUser,checkcompanyurlslugavailiblity)
-router.post("/update-AutoRenewal", isAuthenticatedUser,updateAutoRenewal);
-router.post('/invite/userByCSV', isAuthenticatedUser, inviteTeamMemberByCSV)
-router.get('/company_share_referreldata', isAuthenticatedUser, getcompanies_share_referral_datas)
-router.post("/updatecompany_referral_data", isAuthenticatedUser,updatecompany_referral_data);
-router.post('/user/shippingAddress/add', isAuthenticatedUser, createShippingAddress)
-router.post('/invited/user', invitedUser)
-router.post('/invited/register-user', registerInvitedUser)
-router.post('/invited/google-sign-up', invitedUserGoogleSignup)
-router.post('/reinviteuser', isAuthenticatedUser, resendemailinvitation);
-router.post('/reject-invitation/:invitationToken', rejectInvitation)
+router.post(
+  "/check-availability",
+  isAuthenticatedUser,
+  checkcompanyurlslugavailiblity
+);
+router.post("/update-AutoRenewal", isAuthenticatedUser, updateAutoRenewal);
+router.post("/invite/userByCSV", isAuthenticatedUser, inviteTeamMemberByCSV);
+
+router.get(
+  "/company_share_referreldata",
+  isAuthenticatedUser,
+  getcompanies_share_referral_data
+);
+router.put(
+  "/updatecompany_referral_data",
+  isAuthenticatedUser,
+  updatecompany_referral_data
+);
+
+router.post(
+  "/user/shippingAddress/add",
+  isAuthenticatedUser,
+  createShippingAddress
+);
+router.post("/invited/user", invitedUser);
+router.post("/invited/register-user", registerInvitedUser);
+router.post("/invited/google-sign-up", invitedUserGoogleSignup);
+router.post("/reinviteuser", isAuthenticatedUser, resendemailinvitation);
+router.post("/reject-invitation/:invitationToken", rejectInvitation);
 // router.get("/user/shippingAddresses",isAuthenticatedUser, getAllShippingAddress)
-router.get("/user/all/shippingAddresses", isAuthenticatedUser, getAllShippingAddress)
-router.delete("/user/shippingAddress/remove/:addressId", isAuthenticatedUser, removeShippingAddress)
-router.post("/shippingAddress/edit/:editAddressId",isAuthenticatedUser, editShippingAddress)
-
-
-
+router.get(
+  "/user/all/shippingAddresses",
+  isAuthenticatedUser,
+  getAllShippingAddress
+);
+router.delete(
+  "/user/shippingAddress/remove/:addressId",
+  isAuthenticatedUser,
+  removeShippingAddress
+);
+router.post(
+  "/shippingAddress/edit/:editAddressId",
+  isAuthenticatedUser,
+  editShippingAddress
+);
 
 module.exports = router;
