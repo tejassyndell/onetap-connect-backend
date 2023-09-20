c140babd68fe03b03704dd64019baac5a9f34f78
dev-4 <developersweb004@gmail.com>
2023-09-20 15:29:24 +0530

    Create pm2.config.js

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'highgrove-backend',
+        script: 'app.js', // Entry point of your Node.js application
+        watch: true,
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
