ffb9f3f2fe4549092e48c055785e6b51a36046a6
dev-4 <developersweb004@gmail.com>
2023-09-20 18:54:56 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes/paymentRoutes.js
+++ routes/paymentRoutes/paymentRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   processPayment,
   sendStripeApiKey,
+  createCustomer,
+  createSubscription,
+  switchToManualRenewal
 } = require("../../controllers/paymentController/paymentcontroller");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../../middleware/auth");
 
-router.route("/payment/process").post(processPayment);
+router.post('/payment/process', createSubscription)
+router.post('/payment/create-customer', createCustomer)
+router.post('/payment/subcription', createSubscription)
+router.post('/payment/switchToManualRenewal', switchToManualRenewal)
 
 
 module.exports = router;
