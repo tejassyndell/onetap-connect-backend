d21a524b9f8784f4e40c160c517b50fe99581ab9
DevelopersWeb001 <129718954+DevelopersWeb001@users.noreply.github.com>
2023-09-21 15:39:35 +0530

    changes in test clock remove

--- controllers/paymentController/paymentcontroller.js
+++ controllers/paymentController/paymentcontroller.js
@@ -100,7 +100,7 @@ exports.createCustomer = catchAsyncErrors(async (req, res, next) => {
         country: user.billing_address.country,
         postal_code: user.billing_address.postal_code,
       },
-      test_clock: "clock_1NsM3hHsjFNmmZSiVkQdrD5s",
+      // test_clock: "clock_1NsM3hHsjFNmmZSiVkQdrD5s",
       shipping: {
       name: `${user.first_name} ${user.last_name}`,
         address: {
