c7c4bbb64ab83a54a7034b687a1a7a5f9796bf0b
developer06 <developersweb002@gmail.com>
2023-09-27 16:27:02 +0530

    create api for user information auto save

--- routes/companyRoutes/companyRoutes.js
+++ routes/companyRoutes/companyRoutes.js
@@ -13,7 +13,7 @@ const { login,
      inviteTeamMembermanually,
      uploadImage,
      saveuserdata,
-     savecompanydata, deleteuser} = require('../../controllers/customers/userController.js');
+     savecompanydata, deleteuser, saveuserinfodata} = require('../../controllers/customers/userController.js');
 const {imageUpload} = require('../../middleware/imageUpload');
 
 const router = express.Router();
@@ -143,6 +143,7 @@ router.post('/users/add-manual-user', isAuthenticatedUser, inviteTeamMembermanua
 router.post('/users/image-upload', imageUpload, uploadImage)
 router.post('/save_user_data', isAuthenticatedUser, saveuserdata);
 router.post('/save_company_data', isAuthenticatedUser, savecompanydata);
+router.post('/save_userinfo_data', isAuthenticatedUser, saveuserinfodata);
 router.delete('/deleteuser', isAuthenticatedUser, deleteuser)
 
 
