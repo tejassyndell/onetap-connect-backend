ab0457bd2478183e43c9f3df7a478c4bd2708ebd
DevelopersWeb001 <129718954+DevelopersWeb001@users.noreply.github.com>
2023-10-13 16:13:57 +0530

    remove in usercontroller for multiple entry shippingaddress

--- controllers/customers/userController.js
+++ controllers/customers/userController.js
@@ -2079,7 +2079,7 @@ exports.checkoutHandler = catchAsyncErrors(async (req, res, next) => {
       shipping_address: [shippingData],
     });
   } else {
-    shippingAddressFind.shipping_address.push(shippingData);
+    shippingAddressFind.shipping_address = [shippingData];
   }
 
   const card = await Cards.create(cardData);
@@ -2174,7 +2174,7 @@ exports.checkoutHandlerFree = catchAsyncErrors(async (req, res, next) => {
       shipping_address: [shippingData],
     });
   } else {
-    shippingAddressFind.shipping_address.push(shippingData);
+    shippingAddressFind.shipping_address = [shippingData];
   }
 
   // const card = await Cards.create(cardData);
