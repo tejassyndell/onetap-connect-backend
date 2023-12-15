9a95d94e0ad384b75af60e586637a95e2b872131
DevelopersWeb001 <129718954+DevelopersWeb001@users.noreply.github.com>
2023-12-15 18:38:39 +0530

    changes in ordeschema modal

--- models/NewSchemas/orderSchemaModel.js
+++ models/NewSchemas/orderSchemaModel.js
@@ -67,7 +67,8 @@ const order_Schema = new mongoose.Schema(
         addonId: { type: mongoose.Schema.Types.ObjectId, ref: "otc_addons" },
         status: { type: String , default: 'N/A' },
         assignTo: { type: String , default: '' },
-        price: {type: Number, default: 0} 
+        price: {type: Number, default: 0} ,
+        addonDiscountPrice: {type: Number, default: 0 }
       },
     ],
     subscription_details: {
