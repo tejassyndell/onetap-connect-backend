4a03924dd6d1582461671f8811f623c7164f9adc
tejassyndell <developersweb004@gmail.com>
2023-12-20 16:47:00 +0530

    Created card editor api

--- routes/OTC-AdminRoutes/adminRoutes.js
+++ routes/OTC-AdminRoutes/adminRoutes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const {
   testAPIS, getClients, Signup, OtcLogin, Otclogout, getOtcAdminProfile, getordersclient, getallusers, getallusersofcompany, getcompanyuserstatus, updateAddons, getAddons, createPlan , getPlans,createCategories, getCategories, getOrderssofcompany, updateTeamofuser, updateStatusofuser, updateStatusofcompany, updateClientCompanyInformation, showClientCompanyCardDetails, createCoupon, getCoupon, getUser, otcUpdateUserDetails, otc_getcompanies_share_referral_data, updateRedirectLink, GetSubscriptionDetailsForAdmin, getsubscriptiondetails, AdmininviteTeamMemberByCSV , AdmininviteTeamMember, inviteTeamMembermanuallybyadmin, getinvitedUsersbyadmin, resendemailinvitationbyadmin, getCompanyDetailsforAdmin, checkcompanyurlslugavailiblityAdminside, UpdateCompanySlugFromAdmin, UpdateCompanySettings, getsharereferalSettingsAdmin, UpdateLeadCaptureSettings, getAllOrders, updateOrders, deleteOrders, getSingleOrder, updateOrder
   ,createClient, getActiveUsersOfCompany, getTeamofCompany, updateTeamNamebyAdmin, removeTeamFromUsersByadmin, deleteteamofselectedcompany, renameteamofselectedcompany, createNewteamofselectedcompany, getAllShippingAddressofcompany, createShippingAddressofcompany, removeShippingAddressofcompany, editShippingAddressofcompany, updateuserroleofcompanyusers, updateuserplanonrolechangeofcompany, fetchbillingaddressofcompany, updateBillingAddressofcompany, getallcompanynames, otcadminusers, addAdminUser, updateAdminUser,getcompanyorders, GetorderByCompanyIDandOrderNumber, saveclientTags, getclienttags
-,sendOrderInvoice,
-addreferer,
-getreferer} = require("../../controllers/OTC-AdminController/Clients/clientsController");
+,sendOrderInvoice,addreferer, getreferer, getauser} = require("../../controllers/OTC-AdminController/Clients/clientsController");
 const { isOtcAdminAuthenticatedUser } = require("../../middleware/OtcAdminAuth");
 const { productImageUpload } = require("../../middleware/OTC-AdminProductimageUpload");
 const { createProduct, imageUpload, createProductCategory } = require("../../controllers/OTC-AdminController/Clients/productController");
@@ -30,6 +28,7 @@ router.post("/admin/imageUpload", productImageUpload, imageUpload);
 router.get("/admin/productCategory/fetch", getCategories);
 router.post("/admin/imageUpload", productImageUpload, imageUpload);
 router.get("/admin/users", isOtcAdminAuthenticatedUser, getallusers);
+router.get("/admin/getaUser/:email", getauser);
 router.get("/admin/user/:id", isOtcAdminAuthenticatedUser, getUser);
 router.get("/admin/company_share_referreldata/:id", isOtcAdminAuthenticatedUser, otc_getcompanies_share_referral_data);
 router.get("/admin/companyusers/:id", getallusersofcompany);
