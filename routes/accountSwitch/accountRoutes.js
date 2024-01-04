db8a32c4631e3cfc48133818f75ec3be7a739f59
developerweb004 <140089167+DevelopersWeb004@users.noreply.github.com>
2024-01-04 18:35:41 +0530

    updated accountroutes
    added isauthenticated

--- routes/accountSwitch/accountRoutes.js
+++ routes/accountSwitch/accountRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { isAuthenticatedUser, authorizeRoles } = require("../../middleware/auth.js");
 const { getActiveAccounts, switchAccounts } = require('../../controllers/customers/accountController.js')
-const { isAccountAuthenticated } = require('../../middleware/switchAcAuth')
+const {isAccountAuthenticated} = require('../../middleware/switchAcAuth')
 const router = express.Router();
 
-router.post('/accounts/active', getActiveAccounts)
-router.post('/accounts/switch', switchAccounts)
+router.post('/accounts/active',isAuthenticatedUser,getActiveAccounts)
+router.post('/accounts/switch',isAccountAuthenticated, switchAccounts)
 
 module.exports = router;
